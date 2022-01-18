063332aeaff733be36543c4ca7a6ab2198d6880f
LuanSavarese <66728582+LuanSavares@users.noreply.github.com>
2022-01-18 13:18:09 -0300

    Primeiro
    Primeiro

---
+++ Calculadora.py
@@ -0,0 +1,12 @@
+a = int(input('Digite o primeiro valor: '))
+b = int(input('Digite o segundo valor: '))
+
+soma = a + b
+subtracao = a - b
+multiplicacao = a * b
+divisao = a / b
+
+print('A soma dos valores: {}'.format(soma))
+print('A subtração dos valores: {}'.format(subtracao))
+print('A multiplicação dos valores: {}'.format(multiplicacao))
+print('A divisão dos valores: {}'.format(divisao))
